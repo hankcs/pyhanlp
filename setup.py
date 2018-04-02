43e51e55bd044a1ff2c261baefb3b803bbffe003
hankcs <jfservice@126.com>
2018-04-01 22:04:46 -0500

    Windows 配置文件反斜杠

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name='pyhanlp',
-    version='0.1.30',
+    version='0.1.31',
     description='Python wrapper for HanLP: Han Language Processing',
     long_description=long_description,
     url='https://github.com/hankcs/pyhanlp',
