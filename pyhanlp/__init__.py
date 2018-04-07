577acd76f52143cb532ce78846ed5d0846a8c441
hankcs <jfservice@126.com>
2018-04-07 11:05:27 -0500

    LazyLoading CustomDictionary，提高import速度

--- pyhanlp/__init__.py
+++ pyhanlp/__init__.py
@@ -105,7 +105,33 @@ class SafeJClass(object):
         return SafeJClass(proxy)
 
 
+class LazyLoadingJClass(object):
+    def __init__(self, proxy):
+        """
+        惰性加载Class。仅在实际发生调用时才触发加载，适用于包含资源文件的静态class
+        :param proxy:
+        """
+        self._proxy = proxy
+
+    def __getattr__(self, attr):
+        _attach_jvm_to_thread()
+        self._lazy_load_jclass()
+        return getattr(self._proxy, attr)
+
+    def _lazy_load_jclass(self):
+        if type(self._proxy) is str:
+            self._proxy = JClass(self._proxy)
+
+    def __call__(self, *args):
+        self._lazy_load_jclass()
+        if args:
+            proxy = self._proxy(*args)
+        else:
+            proxy = self._proxy()
+        return SafeJClass(proxy)
+
+
 # API列表
-CustomDictionary = SafeJClass('com.hankcs.hanlp.dictionary.CustomDictionary')
+CustomDictionary = LazyLoadingJClass('com.hankcs.hanlp.dictionary.CustomDictionary')
 HanLP = SafeJClass('com.hankcs.hanlp.HanLP')
 PerceptronLexicalAnalyzer = SafeJClass('com.hankcs.hanlp.model.perceptron.PerceptronLexicalAnalyzer')
