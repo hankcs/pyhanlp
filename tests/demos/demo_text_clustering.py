4c1a5da4f00877600810c6b34431fc1755b75242
hankcs <jfservice@126.com>
2019-02-02 10:46:31 -0500

    新增文本聚类demo

---
+++ tests/demos/demo_text_clustering.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# Author：hankcs
+# Date: 2018-08-18 23:26
+from pyhanlp import *
+
+ClusterAnalyzer = JClass('com.hankcs.hanlp.mining.cluster.ClusterAnalyzer')
+
+if __name__ == '__main__':
+    analyzer = ClusterAnalyzer()
+    analyzer.addDocument("赵一", "流行, 流行, 流行, 流行, 流行, 流行, 流行, 流行, 流行, 流行, 蓝调, 蓝调, 蓝调, 蓝调, 蓝调, 蓝调, 摇滚, 摇滚, 摇滚, 摇滚")
+    analyzer.addDocument("钱二", "爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲")
+    analyzer.addDocument("张三", "古典, 古典, 古典, 古典, 民谣, 民谣, 民谣, 民谣")
+    analyzer.addDocument("李四", "爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 爵士, 金属, 金属, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲, 舞曲")
+    analyzer.addDocument("王五", "流行, 流行, 流行, 流行, 摇滚, 摇滚, 摇滚, 嘻哈, 嘻哈, 嘻哈")
+    analyzer.addDocument("马六", "古典, 古典, 古典, 古典, 古典, 古典, 古典, 古典, 摇滚")
+    print(analyzer.kmeans(3))
+    print(analyzer.repeatedBisection(3))
+    print(analyzer.repeatedBisection(1.0))  # 自动判断聚类数量k
