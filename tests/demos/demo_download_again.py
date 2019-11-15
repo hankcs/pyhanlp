4e0193807d040e905c9ddab9b859df4829634140
hankcs <jfservice@126.com>
2019-11-15 11:30:03 -0500

    演示如何通过自动下载修复失败的的安装

---
+++ tests/demos/demo_download_again.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2019-11-15 11:17
+# 演示如何通过自动下载修复失败的的安装
+from pyhanlp.static import install_hanlp_jar, install_hanlp_data
+
+install_hanlp_jar()
+install_hanlp_data()
