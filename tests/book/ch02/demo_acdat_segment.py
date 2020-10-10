bdaa7c66b1801e4a4d8c10f23126a807a736d010
hankcs <jfservice@126.com>
2020-10-10 13:29:02 -0400

    《自然语言处理入门》2.8 演示AhoCorasickDoubleArrayTrieSegment传入词典路径

--- tests/book/ch02/demo_acdat_segment.py
+++ tests/book/ch02/demo_acdat_segment.py
@@ -8,5 +8,5 @@
 from pyhanlp import *
 
 HanLP.Config.ShowTermNature = False
-segment = JClass('com.hankcs.hanlp.seg.Other.AhoCorasickDoubleArrayTrieSegment')()
+segment = JClass('com.hankcs.hanlp.seg.Other.AhoCorasickDoubleArrayTrieSegment')(HanLP.Config.CoreDictionaryPath)
 print(segment.seg("江西鄱阳湖干枯，中国最大淡水湖变成大草原"))
