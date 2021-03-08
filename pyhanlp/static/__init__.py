835d4fcd77f9dfd21cb06343876997822b7d5410
hankcs <jfservice@126.com>
2021-03-08 18:38:26 -0500

    改善Windows升级体验

--- pyhanlp/static/__init__.py
+++ pyhanlp/static/__init__.py
@@ -60,6 +60,8 @@ INDEX_HTML = os.path.join(STATIC_ROOT, 'index.html')
 def remove_file(filename):
     try:
         os.remove(filename)
+    except PermissionError:
+        pass
     except OSError as e:  # this would be "except OSError, e:" before Python 2.6
         if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
             raise  # re-raise exception if a different error occurred
