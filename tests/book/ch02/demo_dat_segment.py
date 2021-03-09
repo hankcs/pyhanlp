61da386ea3b693680075451816e235c050e39cc3
hankcs <jfservice@126.com>
2021-03-09 12:10:07 -0500

    修复demo_dat_segment.py

--- tests/book/ch02/demo_dat_segment.py
+++ tests/book/ch02/demo_dat_segment.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 # Author：hankcs
 # Date: 2018-05-29 09:48
 # 《自然语言处理入门》2.8 HanLP 的词典分词实现
@@ -9,11 +8,12 @@ from pyhanlp import *
 from pyhanlp.static import HANLP_DATA_PATH
 
 HanLP.Config.ShowTermNature = False
-segment = DoubleArrayTrieSegment()
-print(segment.seg('江西鄱阳湖干枯，中国最大淡水湖变成大草原'))
-
 dict1 = HANLP_DATA_PATH + "/dictionary/CoreNatureDictionary.mini.txt"
 dict2 = HANLP_DATA_PATH + "/dictionary/custom/上海地名.txt ns"
+
+segment = DoubleArrayTrieSegment(dict1)
+print(segment.seg('江西鄱阳湖干枯，中国最大淡水湖变成大草原'))
+
 segment = DoubleArrayTrieSegment([dict1, dict2])
 print(segment.seg('上海市虹口区大连西路550号SISU'))
 
