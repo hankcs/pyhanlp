5c690d68ff4c140963f2d75f18f12cf507509ab9
hankcs <jfservice@126.com>
2021-03-09 13:53:23 -0500

    《自然语言处理入门》随书代码Jupyter格式

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,13 @@
 # -*- coding:utf-8 -*-
 # Author：hankcs
 # Date: 2018-06-03 19:15
+import os
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+
+def cdroot():
+    """
+    cd to project root, so models are saved in the root folder
+    """
+    os.chdir(root)
